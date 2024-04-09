e0ebcf68e4bfaeca62c4de95e62880dbb673d21e
Sabareesh G <123008085+G-Sabareesh@users.noreply.github.com>
2024-04-09 13:59:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function calculateEMI() {
-    var principal = parseFloat(document.getElementById('principal').value);
-    var rate = parseFloat(document.getElementById('rate').value) / 100 / 12; // Annual rate to monthly rate
-  
-    var termType = document.getElementById('termType').value;
-    var time = parseFloat(document.getElementById('time').value);
-    if (termType === 'years') {
-      time *= 12; // Convert years to months
-    }
-  
-    var emi = principal * rate * Math.pow(1 + rate, time) / (Math.pow(1 + rate, time) - 1);
-    var totalAmountPaid = emi * time;
-  
-    document.getElementById('emiResult').innerHTML = "EMI: ₹" + emi.toFixed(2) + "<br>Total Amount Paid: ₹" + totalAmountPaid.toFixed(2);
+  var principal = parseFloat(document.getElementById('principal').value);
+  var rate = parseFloat(document.getElementById('rate').value) / 100 / 12; // Annual rate to monthly rate
+
+  var termType = document.getElementById('termType').value;
+  var time = parseFloat(document.getElementById('time').value);
+  if (termType === 'years') {
+    time *= 12; // Convert years to months
   }
-  
+
+  var emi = principal * rate * Math.pow(1 + rate, time) / (Math.pow(1 + rate, time) - 1);
+  var totalAmountPaid = emi * time;
+  var totalInterestPaid = totalAmountPaid - principal;
+
+  document.getElementById('emiResult').innerHTML = "Monthly EMI: ₹" + emi.toLocaleString('en-IN', { maximumFractionDigits: 2 }) + "<br>Total Interest Paid: ₹" + totalInterestPaid.toLocaleString('en-IN', { maximumFractionDigits: 2 })  + "<br>Total Amount Paid: ₹" + totalAmountPaid.toLocaleString('en-IN', { maximumFractionDigits: 2 } );
+}
